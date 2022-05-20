8889007be1d2a51a06fb7d25fe2633e9f8a30174
Catalin Pirvu <Catalin.Pirvu@viavisolutions.com>
2022-05-20 16:23:12 +0300

    adaugat rezolvari Lab9 FMSFSI

--- LabTDD/js/fizzbuzz.js
+++ LabTDD/js/fizzbuzz.js
@@ -1,6 +1,23 @@
 // Rules here: https://en.wikipedia.org/wiki/Fizz_buzz
 const fizzbuz = (limit) => {
-    return ''
+    let secventa = "";
+    for (let i = 1; i <= limit; i++) {
+        secventa += nextWord(i);
+    }
+    return secventa;
+}
+
+function nextWord(current) {
+    if (current % 15 === 0) { // simplificare pentru ca divide si 3 si 5
+       return "FizzBuzz";
+    }
+    if (current % 3 === 0) {
+        return "Fizz";
+    }
+    if (current % 5 === 0) {
+       return "Buzz";
+    }
+    return current;
 }
 
 module.exports = fizzbuz;
--- LabTDD/js/fizzbuzz.spec.js
+++ LabTDD/js/fizzbuzz.spec.js
@@ -6,34 +6,34 @@ describe('Test suite pentru algoritmul fizzbuz', ()=> {
         expect(fizzbuz(1)).toEqual('1')
     })
 
-    // it('Test pentru val 2', ()=> {
-    //     expect(fizzbuz(2)).toEqual('12')
-    // })
+    it('Test pentru val 2', ()=> {
+        expect(fizzbuz(2)).toEqual('12')
+    })
 
-    // it('Test pentru val 3', ()=> {
-    //     expect(fizzbuz(3)).toEqual('12Fizz')
-    // })
+    it('Test pentru val 3', ()=> {
+        expect(fizzbuz(3)).toEqual('12Fizz')
+    })
 
-    // it('Test pentru val 4', ()=> {
-    //     expect(fizzbuz(4)).toEqual('12Fizz4')
-    // })
+    it('Test pentru val 4', ()=> {
+        expect(fizzbuz(4)).toEqual('12Fizz4')
+    })
 
-    // it('Test pentru val 5', ()=> {
-    //     expect(fizzbuz(5)).toEqual('12Fizz4Buzz')
-    // })
-    // it('Test pentru val 6', ()=> {
-    //     expect(fizzbuz(6)).toEqual('12Fizz4BuzzFizz')
-    // })
-    // it('Test pentru val 9', ()=> {
-    //     expect(fizzbuz(9)).toEqual('12Fizz4BuzzFizz78Fizz')
-    // })
-    // it('Test pentru val 10', ()=> {
-    //     expect(fizzbuz(10)).toEqual('12Fizz4BuzzFizz78FizzBuzz')
-    // })
-    // it('Test pentru val 15', ()=> {
-    //     expect(fizzbuz(15)).toEqual('12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz')
-    // })
-    // it('Test pentru val 16', ()=> {
-    //     expect(fizzbuz(16)).toEqual('12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz16')
-    // })
+    it('Test pentru val 5', ()=> {
+        expect(fizzbuz(5)).toEqual('12Fizz4Buzz')
+    })
+    it('Test pentru val 6', ()=> {
+        expect(fizzbuz(6)).toEqual('12Fizz4BuzzFizz')
+    })
+    it('Test pentru val 9', ()=> {
+        expect(fizzbuz(9)).toEqual('12Fizz4BuzzFizz78Fizz')
+    })
+    it('Test pentru val 10', ()=> {
+        expect(fizzbuz(10)).toEqual('12Fizz4BuzzFizz78FizzBuzz')
+    })
+    it('Test pentru val 15', ()=> {
+        expect(fizzbuz(15)).toEqual('12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz')
+    })
+    it('Test pentru val 16', ()=> {
+        expect(fizzbuz(16)).toEqual('12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz16')
+    })
 })
